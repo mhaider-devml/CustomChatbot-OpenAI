d0adc042d1d40d06218d72865e9195e79146b4e9
Muhammad Ali Haider <130838791+mhaider-devml@users.noreply.github.com>
2023-04-15 17:11:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-
-
 const prompt = document.getElementById("prompt");
 const results = document.getElementById("results");
 
-function callGPTAPI() {
-    const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': '7868651f43msh4eb36e4a4b64bb5p1faed2jsnc991323accbf',
-            'X-RapidAPI-Host': 'openai80.p.rapidapi.com'
-        },
-        body: `{"model":"gpt-3.5-turbo","messages":[{"role":"user","content":"${prompt.value}"}]}`
-    };
+const callGPTAPI = async () => {
+    try {
+        const options = {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                "X-RapidAPI-Key": "7868651f43msh4eb36e4a4b64bb5p1faed2jsnc991323accbf",
+                "X-RapidAPI-Host": "openai80.p.rapidapi.com",
+            },
+            body: `{
+                "model":"gpt-3.5-turbo",
+                "messages":[{"role":"user",
+                            "content":"${prompt.value}"
+                        }]
+                    }`,
+        };
 
-    fetch('https://openai80.p.rapidapi.com/chat/completions', options)
-    // axios('https://openai80.p.rapidapi.com/chat/completions', options)
-        .then(response => response.json())
-        .then(response => {
-            results.innerText = response.choices[0].message.content;
-        })
-        .catch(err => console.error(err));
-}
+        let response = await fetch(
+            "https://openai80.p.rapidapi.com/chat/completions",
+            options
+        );
+        let response1 = await response.json();
+        results.innerText = response1.choices[0].message.content;
+    }
+    catch (error) {
+        console.error(error);
+        results.innerText = "An error occurred. Please try again later.";
+    }
+};
+
